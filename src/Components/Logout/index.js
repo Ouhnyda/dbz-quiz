41645e7ed0973e4851cd9b0bc1e1eb185c97836f
Ouhnyda <dossantos_dany@hotmail.fr>
2021-01-18 02:11:25 +0100

    Creating Welcome, Quiz and Logout; Logout done

---
+++ src/Components/Logout/index.js
@@ -0,0 +1,34 @@
+import React, {useState, useEffect, useContext} from 'react'
+import {FirebaseContext} from '../Firebase'
+
+const Logout = () => {
+
+    const [checked, setChecked] = useState(false)
+
+    const firebase = useContext(FirebaseContext)
+
+    useEffect(() => {
+        if (checked) {
+            firebase.signOutUser();
+        }
+    }, [checked, firebase])
+
+    const handleChange = e => {
+        setChecked(e.target.checked)
+    }
+
+    return (
+        <div className="logoutContainer">
+            <label className="switch">
+
+            <input onChange={handleChange} type="checkbox" checked={checked} />
+
+            <span className="slider round"></span>
+
+            </label>
+            
+        </div>
+    )
+}
+
+export default Logout
--- src/Components/Welcome/index.js
+++ src/Components/Welcome/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import Logout from '../Logout'
+import Quiz from '../Quiz'
 
 const Welcome = () => {
     return (
         <div className="quiz-bg" >
             <div className="container" >
-                Coucou
+                <Logout />
+                <Quiz />
             </div>
 
         </div>
