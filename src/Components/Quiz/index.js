082726e6803f10152e9020e00accab9499908326
Ouhnyda <dossantos_dany@hotmail.fr>
2021-02-05 02:41:05 +0100

    catching Q&R (not finished)

--- src/Components/Quiz/index.js
+++ src/Components/Quiz/index.js
@@ -1,10 +1,59 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Levels from '../Levels'
 import ProgressBar from '../ProgressBar'
+import {quizDbz} from '../QuizDbz'
 
-const Quiz = (props) => {
+class Quiz extends Component {
 
-    const {pseudo, email} = props.userData;
+    state = {
+        levelName: ["debutant",'confirme',"expert"],
+        quizLevel: 0,
+        maxQuestion: 10,
+        storedQuestions: [],
+        question: null,
+        option: [],
+        idQuestion: 0
+    }
+
+    loadQuestions = quiz => {
+
+        const fetchedArrayQuiz = quizDbz[0].quiz[quiz];
+
+        if (fetchedArrayQuiz >= this.state.maxQuestion) {
+
+            // const qui va permettre de "cacher" la réponse via sur ReactDevTools :
+          const newArray = fetchedArrayQuiz.map( ({answer, ...keepRest}) => keepRest );
+
+          this.setState({
+              storedQuestions: newArray
+          })
+
+        }else{
+            console.log("Pas assez de question")
+        }
+    }
+
+    // Methode permettant d'invoquer loadQuestion() :
+
+    componentDidMount() {
+        this.loadQuestions(this.state.levelName[this.state.quizLevel])
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (this.state.storedQuestions !== prevState.storedQuestions) {
+
+            this.setState({
+                question: this.state.storedQuestions[this.state.idQuestion].question,
+                options:  this.state.storedQuestions[this.state.idQuestion].options
+            })
+        }
+    }
+    
+    
+    render(){
+
+
+    const {pseudo, email} = this.props.userData;
 
     return (
         <div>
@@ -23,6 +72,7 @@ const Quiz = (props) => {
             </div>
         </div>
     )
+    }
 }
 
 export default Quiz
---
+++ src/Components/QuizDbz/index.js
@@ -0,0 +1,333 @@
+export const quizDbz = [
+    {
+        fournisseur: "DOS SANTOS Dany",
+        sujet: "Quiz sur l'univers de Dragon Ball Z",
+        quiz:{
+            debutant: [
+                {
+                    id: 1,
+                    question: "Comment s'appelle le personnage principal ?",
+                    options: [
+                        "Son Goku",
+                        "Son Gohan",
+                        "Son Goten",
+                        "Son Gofor",
+                    ],
+                    answer: "Son Goku",
+                },
+                {
+                    id: 2,
+                    question: "Comment s'appelle le personnage principal ?",
+                    options: [
+                        "Son Goku",
+                        "Son Gohan",
+                        "Son Goten",
+                        "Son Gofor",
+                    ],
+                    answer: "Son Goku",
+                },
+                {
+                    id: 3,
+                    question: "Comment s'appelle le personnage principal ?",
+                    options: [
+                        "Son Goku",
+                        "Son Gohan",
+                        "Son Goten",
+                        "Son Gofor",
+                    ],
+                    answer: "Son Goku",
+                },
+                {
+                    id: 4,
+                    question: "Comment s'appelle le personnage principal ?",
+                    options: [
+                        "Son Goku",
+                        "Son Gohan",
+                        "Son Goten",
+                        "Son Gofor",
+                    ],
+                    answer: "Son Goku",
+                },
+                {
+                    id: 5,
+                    question: "Comment s'appelle le personnage principal ?",
+                    options: [
+                        "Son Goku",
+                        "Son Gohan",
+                        "Son Goten",
+                        "Son Gofor",
+                    ],
+                    answer: "Son Goku",
+                },
+                {
+                    id: 6,
+                    question: "Comment s'appelle le personnage principal ?",
+                    options: [
+                        "Son Goku",
+                        "Son Gohan",
+                        "Son Goten",
+                        "Son Gofor",
+                    ],
+                    answer: "Son Goku",
+                },
+                {
+                    id: 7,
+                    question: "Comment s'appelle le personnage principal ?",
+                    options: [
+                        "Son Goku",
+                        "Son Gohan",
+                        "Son Goten",
+                        "Son Gofor",
+                    ],
+                    answer: "Son Goku",
+                },
+                {
+                    id: 8,
+                    question: "Comment s'appelle le personnage principal ?",
+                    options: [
+                        "Son Goku",
+                        "Son Gohan",
+                        "Son Goten",
+                        "Son Gofor",
+                    ],
+                    answer: "Son Goku",
+                },
+                {
+                    id: 9,
+                    question: "Comment s'appelle le personnage principal ?",
+                    options: [
+                        "Son Goku",
+                        "Son Gohan",
+                        "Son Goten",
+                        "Son Gofor",
+                    ],
+                    answer: "Son Goku",
+                }
+            ],
+            confirme: [
+                {
+                    id: 0,
+                    question: "Test confirme",
+                    options: [
+                        "Son Goku",
+                        "Son Gohan",
+                        "Son Goten",
+                        "Son Gofor",
+                    ],
+                    answer: "Son Goku",
+                },
+                {
+                    id: 1,
+                    question: "Comment s'appelle le personnage principal ?",
+                    options: [
+                        "Son Goku",
+                        "Son Gohan",
+                        "Son Goten",
+                        "Son Gofor",
+                    ],
+                    answer: "Son Goku",
+                },
+                {
+                    id: 2,
+                    question: "Comment s'appelle le personnage principal ?",
+                    options: [
+                        "Son Goku",
+                        "Son Gohan",
+                        "Son Goten",
+                        "Son Gofor",
+                    ],
+                    answer: "Son Goku",
+                },
+                {
+                    id: 3,
+                    question: "Comment s'appelle le personnage principal ?",
+                    options: [
+                        "Son Goku",
+                        "Son Gohan",
+                        "Son Goten",
+                        "Son Gofor",
+                    ],
+                    answer: "Son Goku",
+                },
+                {
+                    id: 4,
+                    question: "Comment s'appelle le personnage principal ?",
+                    options: [
+                        "Son Goku",
+                        "Son Gohan",
+                        "Son Goten",
+                        "Son Gofor",
+                    ],
+                    answer: "Son Goku",
+                },
+                {
+                    id: 5,
+                    question: "Comment s'appelle le personnage principal ?",
+                    options: [
+                        "Son Goku",
+                        "Son Gohan",
+                        "Son Goten",
+                        "Son Gofor",
+                    ],
+                    answer: "Son Goku",
+                },
+                {
+                    id: 6,
+                    question: "Comment s'appelle le personnage principal ?",
+                    options: [
+                        "Son Goku",
+                        "Son Gohan",
+                        "Son Goten",
+                        "Son Gofor",
+                    ],
+                    answer: "Son Goku",
+                },
+                {
+                    id: 7,
+                    question: "Comment s'appelle le personnage principal ?",
+                    options: [
+                        "Son Goku",
+                        "Son Gohan",
+                        "Son Goten",
+                        "Son Gofor",
+                    ],
+                    answer: "Son Goku",
+                },
+                {
+                    id: 8,
+                    question: "Comment s'appelle le personnage principal ?",
+                    options: [
+                        "Son Goku",
+                        "Son Gohan",
+                        "Son Goten",
+                        "Son Gofor",
+                    ],
+                    answer: "Son Goku",
+                },
+                {
+                    id: 9,
+                    question: "Comment s'appelle le personnage principal ?",
+                    options: [
+                        "Son Goku",
+                        "Son Gohan",
+                        "Son Goten",
+                        "Son Gofor",
+                    ],
+                    answer: "Son Goku",
+                },
+            ],
+            expert: [
+                {
+                    id: 0,
+                    question: "test expert",
+                    options: [
+                        "Son Goku",
+                        "Son Gohan",
+                        "Son Goten",
+                        "Son Gofor",
+                    ],
+                    answer: "Son Goku",
+                },
+                {
+                    id: 1,
+                    question: "Comment s'appelle le personnage principal ?",
+                    options: [
+                        "Son Goku",
+                        "Son Gohan",
+                        "Son Goten",
+                        "Son Gofor",
+                    ],
+                    answer: "Son Goku",
+                },
+                {
+                    id: 2,
+                    question: "Comment s'appelle le personnage principal ?",
+                    options: [
+                        "Son Goku",
+                        "Son Gohan",
+                        "Son Goten",
+                        "Son Gofor",
+                    ],
+                    answer: "Son Goku",
+                },
+                {
+                    id: 3,
+                    question: "Comment s'appelle le personnage principal ?",
+                    options: [
+                        "Son Goku",
+                        "Son Gohan",
+                        "Son Goten",
+                        "Son Gofor",
+                    ],
+                    answer: "Son Goku",
+                },
+                {
+                    id: 4,
+                    question: "Comment s'appelle le personnage principal ?",
+                    options: [
+                        "Son Goku",
+                        "Son Gohan",
+                        "Son Goten",
+                        "Son Gofor",
+                    ],
+                    answer: "Son Goku",
+                },
+                {
+                    id: 5,
+                    question: "Comment s'appelle le personnage principal ?",
+                    options: [
+                        "Son Goku",
+                        "Son Gohan",
+                        "Son Goten",
+                        "Son Gofor",
+                    ],
+                    answer: "Son Goku",
+                },
+                {
+                    id: 6,
+                    question: "Comment s'appelle le personnage principal ?",
+                    options: [
+                        "Son Goku",
+                        "Son Gohan",
+                        "Son Goten",
+                        "Son Gofor",
+                    ],
+                    answer: "Son Goku",
+                },
+                {
+                    id: 7,
+                    question: "Comment s'appelle le personnage principal ?",
+                    options: [
+                        "Son Goku",
+                        "Son Gohan",
+                        "Son Goten",
+                        "Son Gofor",
+                    ],
+                    answer: "Son Goku",
+                },
+                {
+                    id: 8,
+                    question: "Comment s'appelle le personnage principal ?",
+                    options: [
+                        "Son Goku",
+                        "Son Gohan",
+                        "Son Goten",
+                        "Son Gofor",
+                    ],
+                    answer: "Son Goku",
+                },
+                {
+                    id: 9,
+                    question: "Comment s'appelle le personnage principal ?",
+                    options: [
+                        "Son Goku",
+                        "Son Gohan",
+                        "Son Goten",
+                        "Son Gofor",
+                    ],
+                    answer: "Son Goku",
+                },
+            ]
+        }
+    }
+]
